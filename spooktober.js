9eba4ade3ec0f1d432ba47076b1f64b80ece926e
Patryk Rajm <patrykrajm06@gmail.com>
2020-10-02 14:37:29 +0200

    Finished version 1.0 code

---
+++ spooktober.js
@@ -0,0 +1,17 @@
+const countDownDate = new Date("Nov 1, 2020 00:00:00").getTime();
+const x = setInterval(function () {
+
+    let now = new Date().getTime();
+    let distance = countDownDate - now;
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    document.getElementById("time").innerHTML = days + "d " + hours + "h " +
+        minutes + "m " + seconds + "s ";
+
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("time").innerHTML = "Spooktober ended :(";
+    }
+}, 1000);
